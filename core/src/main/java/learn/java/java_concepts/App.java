57574206cf9e2db510b8076f49c2c5d889228a84
Amandeep <amandeep.cs@gmail.com>
2018-11-18 14:57:41 +0530

    non static block in anonymous class

--- core/src/main/java/learn/java/java_concepts/App.java
+++ core/src/main/java/learn/java/java_concepts/App.java
@@ -9,6 +9,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        AbstractClass abstractClass = new AbstractClass() {
+            @Override
+            public void overrideMe() {
+                System.out.println("wont be called untill called");
+            }
+
+            {
+            String param = "Hello there";
+            callMe(param);
+        }
+        };
     }
     
     
