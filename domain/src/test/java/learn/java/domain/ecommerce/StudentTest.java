6868a6e98565025ade154bf1c8f5d5a827a2fc52
amandeep2github <amandeep.cs@gmail.com>
2019-04-26 12:30:46 +0530

    Big decimal and predicates

---
+++ domain/src/test/java/learn/java/domain/ecommerce/StudentTest.java
@@ -0,0 +1,28 @@
+package learn.java.domain.ecommerce;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.function.DoublePredicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StudentTest {
+    double units ;
+    private DoublePredicate predicateTest = d -> d == 0;
+
+    @Test
+    public void testApacheEqualsBuilder(){
+        Student stu1 = new Student("Amandeep", 50);
+        Student stu2 = new Student("Amandeep", 50);
+        assertThat(stu1.equals(stu2)).isEqualTo(true);
+
+        assertThat(units == 0.0d).isEqualTo(true);
+        assertThat(predicateTest.test(units)).isEqualTo(true);
+
+        assertThat(BigDecimal.valueOf(1.445354).setScale(5, BigDecimal.ROUND_HALF_EVEN).doubleValue()).isEqualTo(1.44535);
+        assertThat(BigDecimal.valueOf(1.445356).setScale(5, BigDecimal.ROUND_HALF_EVEN).doubleValue()).isEqualTo(1.44536);
+
+    }
+
+}
